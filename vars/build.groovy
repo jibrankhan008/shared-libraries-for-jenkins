67c5f29321cfe674766f9db5322332a62f8b292c
jibrankhan008 <itsjibran008@gmail.com>
2026-06-05 14:02:39 +0500

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def call(String projectName, String dockerhubUser, String imageTag){
+def call(String projectName, String dockerHubUser, String imageTag){
               sh "docker build -t ${dockerhubUser}/frontend-${projectName}:${imageTag} ./frontend"
               sh "docker build -t ${dockerhubUser}/backend-${projectName}:${imageTag} ./backend"
 }
